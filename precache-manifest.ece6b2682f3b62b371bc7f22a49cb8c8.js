adbf1cb6144063d3dc9f5239d8d7ed3d9887a1f8
Daniel <caste0322@gmail.com>
2022-09-08 10:29:23 -0500

    Updates

---
+++ precache-manifest.ece6b2682f3b62b371bc7f22a49cb8c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05f65d3b1564d9d68488",
+    "url": "/to-do-machine-ReactJs/static/css/main.b8cc1167.chunk.css"
+  },
+  {
+    "revision": "05f65d3b1564d9d68488",
+    "url": "/to-do-machine-ReactJs/static/js/main.05f65d3b.chunk.js"
+  },
+  {
+    "revision": "babe19336da30ea606cd",
+    "url": "/to-do-machine-ReactJs/static/js/1.babe1933.chunk.js"
+  },
+  {
+    "revision": "dbeec19e54042979b862",
+    "url": "/to-do-machine-ReactJs/static/js/runtime~main.dbeec19e.js"
+  },
+  {
+    "revision": "6048322bbb846f9737f1b953195e7f1d",
+    "url": "/to-do-machine-ReactJs/index.html"
+  }
+];
